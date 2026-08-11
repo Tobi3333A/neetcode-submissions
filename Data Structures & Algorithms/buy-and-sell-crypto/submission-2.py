ab96613e431aaffcd3c1ad916a85f650bdb44dd4
Oluwatobiloba Adejumo <tobiadejumo10@gmail.com>
2026-08-10 23:07:21 -0500

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        max_profit = 0
+
+        for right in range(len(prices)):
+            if prices[right] < prices[left]:
+                left = right
+            else:
+                profit = prices[right] - prices[left]
+            if profit > max_profit:
+                max_profit = profit
+        return max_profit
