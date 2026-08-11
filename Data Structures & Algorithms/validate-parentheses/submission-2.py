fb274a6db97c0d3f1671bb0b68eb51e352343ac1
Oluwatobiloba Adejumo <tobiadejumo10@gmail.com>
2026-08-11 11:55:22 -0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dictt = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        arr = []
+
+        if len(s) % 2 != 0:
+            return False
+
+        for i in s:
+            if i in dictt:
+                arr.append(i)
+            else:
+                if not arr:
+                    return False
+                if dictt[arr.pop()] != i:
+                    return False
+        return not arr
